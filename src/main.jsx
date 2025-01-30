4cd95a7d9e38296f47962ce10084d3140c3b3bbc
Satyam <sm2003satyam@gmail.com>
2025-01-30 11:46:25 +0530

    update title in index.html and simplify main.jsx rendering

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,7 @@ import { Provider } from 'react-redux'
 import store from './redux/store'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
     <Provider store={store}>
-     
      <App />
-     
-     </Provider>
-  </StrictMode>,
+    </Provider>
 )
